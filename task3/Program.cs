699fa4f3d3d5931ccc3bc632b668719cafb53c3b
Vika <vpredko@mail.ru>
2022-07-24 20:18:23 +0300

    Solve task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿int SumNumbers(int start, int end)
+{
+    if (start == 0)
+    {
+        return end+1;
+    }
+    if (start > 0 && end == 0)
+    {
+        return SumNumbers(start-1,1);
+    }
+    if (start >0 && end>0)
+    {
+        return SumNumbers(start-1, SumNumbers(start, end-1));
+    }
+    {
+        return 0;
+    }
+}
+Console.WriteLine ("Введите натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumbers(m,N));
