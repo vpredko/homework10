804e32e67da688bddf27ab2a709673b633f79994
Vika <vpredko@mail.ru>
2022-07-24 19:23:20 +0300

    Solve task1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿string PrintNumbersRecusiveType(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    {
+        return (start + " " + PrintNumbersRecusiveType(start+1,end));
+    }
+}
+Console.WriteLine ("Введите натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumbersRecusiveType(m,N));
